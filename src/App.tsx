47fd42247bfeaf84f92597070154e65358c76228
Evgenii <evgusa83@gmail.com>
2024-01-21 16:05:16 +0300

    update Slogan, added libery Typewriter

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,6 @@ function App() {
                 <Contact />
                 <Slogan />
                 <Footer />
-
-
         </div>
     );
 }
--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -3,12 +3,27 @@ import styled from "styled-components";
 import {SectionTitle} from "../../../Components/SectionTitle";
 import {Button} from "../../../Components/Button";
 import {FlexContainer} from "../../../Components/FlexContainer";
+import Typewriter from 'typewriter-effect';
+
+
+
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
             <FlexContainer direction={'column'} justify={'centre'} aline={'center'}>
-            <SectionTitle>This is Ilon Mask</SectionTitle>
+            <TypewriterStyled>
+                <Typewriter
+                    options={{
+                        strings: "Crafting Tomorrow, Line by Line",
+                        autoStart: true,
+                        loop: true,
+                        // @ts-ignore
+                        pauseFor: 2500
+      ,
+                    }}
+                />
+            </TypewriterStyled>
             <Button as={'a'} target={'_blank'} href={'https://twitter.com/elonmusk?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'}>Check what</Button>
             </FlexContainer>
             </StyledSlogan>
@@ -20,3 +35,6 @@ const StyledSlogan = styled.section`
     
 `
 
+const  TypewriterStyled = styled(SectionTitle)`
+    font-size: 0.9em;
+`
